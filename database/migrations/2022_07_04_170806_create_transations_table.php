2e6c8faa76f170e7b5f6e5914ce7c4fef52a5c5a
codejutsu1 <codejutsu@protonmail.com>
2022-07-04 18:12:00 +0100

    created the user and transaction table

---
+++ database/migrations/2022_07_04_170806_create_transations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('amount');
+            $table->string('year');
+            $table->boolean('status')->nullable();
+            $table->boolean('paid')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transations');
+    }
+};
